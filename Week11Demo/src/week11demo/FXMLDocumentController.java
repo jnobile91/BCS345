06ba4a0d601a51749c5cf8764a4d737776aa0372
jnobile91 <jnobile91@live.com>
2021-04-16 19:51:12 -0400

    Week 11 Lecture Notes

---
+++ Week11Demo/src/week11demo/FXMLDocumentController.java
@@ -0,0 +1,67 @@
+package week11demo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class FXMLDocumentController implements Initializable {
+    @FXML
+    private Button mZoom;
+    @FXML
+    private TextArea mEditor;
+    
+    int txtSize = 12;
+    
+    Stage stg;
+
+    @FXML
+    private MenuItem mOpenFile;
+    @FXML
+    private Button mZoomOut;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+    @FXML
+    private void mOpenTextFileFunc(ActionEvent event) throws FileNotFoundException {
+        FileChooser fs = new FileChooser();
+        File file = fs.showOpenDialog(null);
+        if (file != null) {
+            openFile(file);
+        }
+    }
+
+    private void openFile(File file) throws FileNotFoundException {
+        String str = "";
+        Scanner sc = new Scanner(file);
+        while (sc.hasNextLine()) {
+            str += sc.nextLine();
+        }
+        mEditor.setText(str);
+    }
+
+    @FXML
+    private void mZoomInOnText(ActionEvent event) {
+        mEditor.setFont(new Font(txtSize++));
+    }
+    
+    @FXML
+    private void mZoomOutOnText(ActionEvent event) {
+        mEditor.setFont(new Font(txtSize--));
+    }
+}
